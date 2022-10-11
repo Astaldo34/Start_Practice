49c361e61ecb272607f8da7ef204310997d4a774
Astaldo <astaldo07@mail.ru>
2022-10-11 21:22:42 +0300

    done code

--- task/Program.cs
+++ task/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿string[] ParseArray(string[] inputArray)
+{
+    int n = 0;
+    int array_length = 0;
+    foreach (var item in inputArray) if (item.Length <= 3) array_length++;
+    string[] resultArray = new string[array_length];
+    foreach (var item in inputArray)
+    {
+        if (item.Length <= 3) {
+            resultArray[n] = item;
+            n++;
+        }
+    }
+
+    return resultArray;
+}
+string[] firstSome = {"Moscow", "London", "Lin", "bl", "GI"};
+
+Console.WriteLine($"[{String.Join(", ", ParseArray(firstSome))}]");
+
+
